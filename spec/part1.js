69b8cc765b2a7c2c693e2edc866869650ef86658
scottbarclay <scottdbarclay@gmail.com>
2017-12-18 20:43:32 -0800

    Add adjustment to test on uniq function regarding sorted array. Removed iterator var.

--- spec/part1.js
+++ spec/part1.js
@@ -343,11 +343,12 @@
         expect(_.uniq(numbers)).to.eql([1, 2, 3, 4]);
       });
 
+      //commenting out iterator var after discussing with HiR
       it('should handle iterators that work with a sorted array', function() {
-        var iterator = function(value) { return value === 1; };
+        //var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(numbers, true, iterator)).to.eql([1, 2]);
+        expect(_.uniq(numbers, isSorted)).to.eql([1, 2, 3, 4]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
