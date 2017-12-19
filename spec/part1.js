fd19fb8b5fba01442e47deaa7f6d6876f4ab7f69
scottbarclay <scottdbarclay@gmail.com>
2017-12-18 18:39:47 -0800

    Add fix to uniq test call

--- spec/part1.js
+++ spec/part1.js
@@ -347,7 +347,7 @@
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(numbers, true, iterator)).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
@@ -362,7 +362,7 @@
       checkForNativeMethods(function() {
         _.map([1, 2, 3, 4], function(num) {
           return num * 2;
-        })
+        });
       });
 
       it('should not mutate the input array', function() {
