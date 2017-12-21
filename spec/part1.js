bed3241f2042f3c3e14e51c49d7dafcec797d092
scottbarclay <scottdbarclay@gmail.com>
2017-12-20 18:34:38 -0800

    Fix uniq call to exclude sort condition

--- spec/part1.js
+++ spec/part1.js
@@ -348,7 +348,7 @@
         //var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(numbers, isSorted)).to.eql([1, 2, 3, 4]);
+        expect(_.uniq(numbers)).to.eql([1, 2, 3, 4]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
